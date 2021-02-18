010b924699c67a21678e712da5baa8c8e797d03a
jpb34 <jpb34@cs-ssh.cs.calvin.edu>
2021-02-18 13:56:39 -0500

    Homework02: added error messaging with stderr

--- homework02/copy.c
+++ homework02/copy.c
@@ -10,35 +10,34 @@ int main(int argc, char *argv[]) {
     printf("E.g: ./copy fileA fileB\n");
 
     // https://www.educba.com/stderr-in-c/
-    fprintf(stderr, "Expected 2 arguments, but only %d were given.\n", (argc - 1));
+    fprintf(stderr, "Expected 2 arguments, but %d were given.\n", (argc - 1));
     return -1;
   }
-
+  
   // https://stackoverflow.com/questions/230062/whats-the-best-way-to-check-if-a-file-exists-in-c
   // ensure source file exists
-  if ( access( argv[1], F_OK ) == 0 ) {
-    printf("Source file exists!\n");
-  } else {
-    printf("Source file does not exist!\n");
+  if ( access( argv[1], F_OK ) != 0 ) {
+    fprintf(stderr, "Specified source file does not exist\n");
     return -1;
   }
 
   // ensure destination file does not exist
-  if ( access ( argv[2], F_OK ) == -1 ) {
-    printf("Destination file does not exist!\n");
-  } else {
-    printf("Destination file exists!\n");
+  if ( access ( argv[2], F_OK ) != -1 ) {
+    fprintf(stderr, "Destination file given already exists.\n");
     return -1;
   }
 
+  
   // ensure that given source file is not a symlink, not a directory, and is just a normal file
   //https://stackoverflow.com/questions/328944/how-do-i-check-if-a-file-is-a-regular-file
   struct stat sb;
 
   // we use lstat, because stat by default will ignore a symlink and will call it regular
-  if (lstat( argv[1], &sb ) == 0 && S_ISREG(sb.st_mode)) {
-    printf("Source file is regular\n");
+  if (lstat( argv[1], &sb ) != 0 || !S_ISREG(sb.st_mode)) {
+    fprintf(stderr, "Source file is not a regular file.\n");
+    return -1;
   }
+
 
   // open source file for reading and destination file for writing
   // https://fresh2refresh.com/c-programming/c-file-handling/fgetc-function-c/ 
@@ -46,20 +45,29 @@ int main(int argc, char *argv[]) {
   FILE *destinationFile = fopen(argv[2], "w");
 
   if ( sourceFile == NULL ) {
-    printf("Source file could not be opened...\n");
+    fprintf(stderr, "Source file could not be opened.\n");
     return -1;
   }
+
   
+  // Loop through each character in the source file and write to destination file
   while (1) {
+    // get character from sourceFile
     char c = fgetc(sourceFile);
+
+    // if character is EOF, break since we are finished
     if (c == EOF)
       break ;
+
+    // put character into destination file
     fputc(c, destinationFile);
   }
 
+  
   // close files
   fclose(sourceFile);
   fclose(destinationFile);
+
   
   return 0;
 }
