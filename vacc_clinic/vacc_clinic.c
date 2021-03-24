bbd4c87dbda8875dbb363ab400813bda394f3b54
jpb34 <jpb34@cs-ssh.cs.calvin.edu>
2021-03-24 18:01:18 -0400

    Homework 05 (Vaccine Clinic): 1 Client and Nurse

--- vacc_clinic/vacc_clinic.c
+++ vacc_clinic/vacc_clinic.c
@@ -18,6 +18,11 @@
 #define NUM_REGISTRATIONS_SIMULTANEOUSLY 4
 
 /* global variables */
+sem_t nurse_ready;
+sem_t client_ready;
+sem_t vaccine_storage;
+
+int vials_left = NUM_VIALS;
 
 int get_rand_in_range(int lower, int upper) {
     return (rand() % (upper - lower + 1)) + lower;
@@ -37,6 +42,7 @@ char *curr_time_s() {
 void walk(int lower, int upper) {
     // TODO: fill in code here.  Use usleep() and get_rand_in_range() from
     // above.
+    usleep(get_rand_in_range(lower, upper)*1000000);
 }
 
 // arg is the nurses station number.
@@ -45,6 +51,20 @@ void *nurse(void *arg) {
 
     fprintf(stderr, "%s: nurse %ld started\n", curr_time_s(), id);
 
+    // Walk to vaccine
+    walk(3, 10);
+
+    // check if no more vials, if not exit clinic
+    sem_wait(&vaccine_storage);
+    if (vials_left == 0) {
+      fprintf(stderr, "%s: nurse %ld realizes there are no more vaccines left, and goes home to party\n", curr_time_s(), id);
+      pthread_exit(NULL);
+    };
+    sem_post(&vaccine_storage);
+    
+    sem_wait(&client_ready);
+    sem_post(&nurse_ready);
+    
     fprintf(stderr, "%s: nurse %ld is done\n", curr_time_s(), id);
 
     pthread_exit(NULL);
@@ -56,6 +76,11 @@ void *client(void *arg) {
     fprintf(stderr, "%s: client %ld has arrived and is walking to register\n",
             curr_time_s(), id);
 
+    walk(3, 10);
+    
+    sem_post(&client_ready);
+    sem_wait(&nurse_ready);
+    
     fprintf(stderr, "%s: client %ld leaves the clinic!\n", curr_time_s(), id);
 
     pthread_exit(NULL);
@@ -63,6 +88,28 @@ void *client(void *arg) {
 
 int main() {
     srand(time(0));
+
+    sem_init(&vaccine_storage, 1, 1); /* vaccines start as available */
+    sem_init(&client_ready, 1, 0);
+    sem_init(&nurse_ready, 1, 0);
+    
+    pthread_t client_thread_id;
+
+    int client_t = pthread_create(&client_thread_id, NULL, client, NULL);
+
+    if (client_t == 0) {
+      /* fprintf(stderr, "Success!"); */
+    }
+
+
+    pthread_t nurse_thread_id;
+
+    int nurse_t = pthread_create(&nurse_thread_id, NULL, nurse, NULL);
+
+    if (nurse_t == 0) {
+    	/* fprintf(stderr, "Success! A nurse has been born to us in a lowly manger"); */
+    }
+
 
     pthread_exit(0);
 }
