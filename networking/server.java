5735f469c095077f5b974c9ed6eb61f20d4c4550
jpb34 <jpb34@goldvm01.cs.calvin.edu>
2021-04-29 19:14:29 -0400

    Server now responds with ceasar cipher rotation

--- networking/server.java
+++ networking/server.java
@@ -12,15 +12,69 @@ public class Server {
 	    this.socket = socket;
 	}
 
+	private void sendMessage(DataOutputStream send, String data) throws IOException {
+	    send.writeBytes(data+"\n");
+	    send.flush();
+	}
+
+	private String shiftString(String input, int shift) {
+	    String shiftedString = "";
+	    for (int i = 0; i < input.length(); i++) {
+		char character = input.charAt(i);
+		if (Character.isLetter(character)) {
+		    int num = ((int) character);
+		    int startingNumber = 0;
+		    
+		    if (num >= 65 && num <= 90) { // uppercase range
+			startingNumber = 65;
+		    } else { // lowercase letters
+			startingNumber = 97;
+		    }
+
+		    int shifted = (num + shift - startingNumber) % 26 + startingNumber;
+		    character = (char) shifted;
+		}
+
+		shiftedString += character;
+		
+	    }
+	    return shiftedString;
+	}
+	
 	public void run() {
-	    System.out.println("Someone connected!");
+
+	    boolean gettingRotation = true;
+	    
 	    try (DataOutputStream send = new DataOutputStream(this.socket.getOutputStream());
 	     BufferedReader recv = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));) {
-		while (true) {
-		    String results = recv.readLine();
-		    send.writeBytes(results+"\n");
-		    send.flush();
+
+		String results = null;
+		int rotation = -1;
+		
+		while ((results = recv.readLine()) != null) {
+		    
+		    if (gettingRotation) {
+			try {
+			    int rotationInput = Integer.parseInt(results);
+			    if (rotationInput <= 25 && rotationInput >= 1) {
+				sendMessage(send, results);
+				gettingRotation = false;
+				rotation = rotationInput;
+			    } else {
+				sendMessage(send, "Invalid rotation #, please supply a number between 1 and 25");
+			    }
+			} catch (Exception e) {
+			    sendMessage(send, "Rotation # must be an Integer!");
+			}
+		    } else {
+			sendMessage(send, shiftString(results, rotation));
+		    }
+
 		}
+
+		recv.close();
+		send.close();
+		this.socket.close();
 		
 	    } catch (Exception e) {
 		System.out.println(e);
