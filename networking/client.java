1d410f2dcfca3f5321c863d2e73fe5a55088ff95
jpb34 <jpb34@goldvm01.cs.calvin.edu>
2021-04-29 21:04:53 -0400

    Added server timestamp and client ip output

--- networking/client.java
+++ networking/client.java
@@ -1,107 +1,121 @@
 /*
+  Ceasar Cipher Client
+
   student: Jacob Brink
   date: 4/29/2021
   lab: networking
 
-  https://docs.oracle.com/javase/tutorial/networking/sockets/readingWriting.html
+  Sources used: https://docs.oracle.com/javase/tutorial/networking/sockets/readingWriting.html
 */
 
 import java.net.*;
 import java.util.Scanner;
 import java.io.*;
 
-
 public class Client {
 
-
-    /* checkRotation
-     * @params: server rotation response and our sent rotation
-     * @returns: true if server response is good, false if server's response fails
-     */
-    public static boolean checkRotation(String rotationResponse, String sentRotation) {       
-	if (rotationResponse != null) {
-	    if (rotationResponse.equals(sentRotation)) {
-		return true;
-	    } else {	
-		System.out.println("The server gave back an incorrect rotation!");
-	    }
-	}	
-	return false;
-    }
-
-    /* prompt
-     * @params: none
-     * @description: sends user's messages to ceasar cipher server and returns server's response
-     */
-    public static void prompt() {
-
-	// Here we open resources in the try with statement so all resources opened are closed correctly
-	try (Socket sock = new Socket(host, port);
-	     DataOutputStream send = new DataOutputStream(sock.getOutputStream());
-	     BufferedReader recv = new BufferedReader(new InputStreamReader(sock.getInputStream()));) {	    
-
-	
-	Scanner scan = new Scanner(System.in);
-	boolean rotationStepComplete = false;
-	int rotation = -1;
-	
-	// the prompt for the user
-	while (true) {
-	    
-	    System.out.print(rotationStepComplete ? "Enter text: " : "Enter rotation:");
-
-	    String input = scan.nextLine();
-	    
-	    if (input.equals("quit"))
-		break;
-
-	    if (rotationStepComplete) {
-		sendReceive(send, recv, input); // send text to server and get output back
-	    } else {
-
-		try {
-		    int rotationInput = Integer.parseInt(input);
-		    if (rotationInput > 25 || rotationInput < 1) {
-			System.out.println("Rotation given is not between 1 and 25");
-			continue;
-		    }
-		} catch (Exception e) {
-		    System.out.println("Rotation must be a number");
-		    continue;
+	/*
+	 * checkRotation
+	 * 
+	 * @params: server rotation response and our sent rotation
+	 * 
+	 * @returns: true if server response is good, false if server's response fails
+	 */
+	public static boolean checkRotation(String rotationResponse, String sentRotation) {
+		if (rotationResponse != null) {
+			if (rotationResponse.equals(sentRotation)) {
+				return true;
+			} else {
+				System.out.println("The server gave back an incorrect rotation!");
+			}
 		}
-
-		String results = sendReceive(send, recv, input);
-				
-		
-		if (checkRotation(results, input)) {
-		    rotationStepComplete = true; // if response is valid, proceed to ceasar cipher text sending and receiving (sometimes the example server gave back incorrect numbers)
-		}
-	    }
+		return false;
 	}
-	} catch (Exception e) {
-	    System.out.println(e);
-	};
-    }
 
-    /* sendReceive
-     * @params: send stream, receive stream, and the data to send
-     * @returns: returns result or throws an error
-     */
-    public static String sendReceive(DataOutputStream send, BufferedReader recv, String data) throws Exception {
-	send.writeBytes(data+"\n");
-	send.flush();
-	String results = recv.readLine();
-	System.out.println("\n\tServer says: " + results + "\n");
-	return results;
-    }
-    
-    public static String host;
-    public static int port;
-    
-    public static void main(String []args) {
-	System.out.println("Welcome to the ceasar cipher!");
-	host = args[0];
-	port = Integer.parseInt(args[1]);
-	prompt();
-    }
+	/*
+	 * prompt
+	 * 
+	 * @params: none
+	 * 
+	 * @description: sends user's messages to ceasar cipher server and returns
+	 * server's response
+	 */
+	public static void prompt() {
+
+		// Here we open resources in the try with statement so all resources opened are
+		// closed correctly
+		try (Socket sock = new Socket(host, port);
+				DataOutputStream send = new DataOutputStream(sock.getOutputStream());
+				BufferedReader recv = new BufferedReader(new InputStreamReader(sock.getInputStream()));) {
+
+			Scanner scan = new Scanner(System.in);
+			boolean rotationStepComplete = false;
+			int rotation = -1;
+
+			// the prompt for the user
+			while (true) {
+
+				System.out.print(rotationStepComplete ? "Enter text: " : "Enter rotation:");
+
+				String input = scan.nextLine();
+
+				if (input.equals("quit"))
+					break;
+
+				if (rotationStepComplete) {
+					sendReceive(send, recv, input); // send text to server and get output back
+				} else {
+
+					try {
+						int rotationInput = Integer.parseInt(input);
+						
+						if (rotationInput > 25 || rotationInput < 1) {
+							System.out.println("Rotation given is not between 1 and 25");
+							continue;
+						}
+
+					} catch (Exception e) {
+						System.out.println("Rotation must be a number");
+						continue;
+					}
+
+					String results = sendReceive(send, recv, input);
+
+					// if response is valid, proceed to ceasar cipher text sending and
+					// receiving (sometimes the example server gave back incorrect numbers)
+					if (checkRotation(results, input)) {
+						rotationStepComplete = true; 
+					}
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		;
+	}
+
+	/*
+	 * sendReceive
+	 * 
+	 * @params: send stream, receive stream, and the data to send
+	 * 
+	 * @returns: returns result or throws an error
+	 */
+	public static String sendReceive(DataOutputStream send, BufferedReader recv, String data) throws Exception {
+		send.writeBytes(data + "\n");
+		send.flush();
+		String results = recv.readLine();
+		System.out.println("\n\tServer says: " + results + "\n");
+		return results;
+	}
+
+	public static String host;
+	public static int port;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the ceasar cipher!");
+		host = args[0];
+		port = Integer.parseInt(args[1]);
+		prompt();
+	}
 }
--- networking/server.java
+++ networking/server.java
@@ -1,125 +1,156 @@
+/*
+  Ceasar Cipher Server
+  student: Jacob Brink
+  date: 4/29/2021
+  lab: networking
+
+  Sources used: https://docs.oracle.com/javase/tutorial/networking/sockets/readingWriting.html
+*/
+
 import java.net.*;
 import java.util.Scanner;
 import java.io.*;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class Server {
 
-    // CeasarWorker handles one connected socket to the server
-    static class CeasarWorker extends Thread {
+	// CeasarWorker handles one connected socket to the server
+	static class CeasarWorker extends Thread {
 
-	private Socket socket;
-    
-	public CeasarWorker(Socket socket) {
-	    this.socket = socket;
-	}
+		private Socket socket;
 
-	/* sendMessage
-         * @params: send stream, data string
-         * @description: sends data to the client socket
-         */
-	private void sendMessage(DataOutputStream send, String data) throws IOException {
-	    send.writeBytes(data+"\n");
-	    send.flush();
-	}
-
-	/* shiftString
-         * @params: input, shift (rotation)
-         * @returns: string that has only the letters shifted by given shift
-         */
-	private String shiftString(String input, int shift) {
-	    String shiftedString = "";
-
-	    for (int i = 0; i < input.length(); i++) {
-		char character = input.charAt(i);
-		if (Character.isLetter(character)) {
-		    int num = ((int) character);
-		    int startingNumber = 0;
-		    
-		    if (num >= 65 && num <= 90) { // uppercase range
-			startingNumber = 65;
-		    } else { // lowercase letters
-			startingNumber = 97;
-		    }
-
-		    int shifted = (num + shift - startingNumber) % 26 + startingNumber;
-		    character = (char) shifted;
+		public CeasarWorker(Socket socket) {
+			this.socket = socket;
 		}
 
-		shiftedString += character;
-		
-	    }
-	    return shiftedString;
-	}
+		/*
+		 * sendMessage
+		 * 
+		 * @params: send stream, data string
+		 * 
+		 * @description: sends data to the client socket
+		 */
+		private void sendMessage(DataOutputStream send, String data) throws IOException {
+			send.writeBytes(data + "\n");
+			send.flush();
+		}
 
-	/* run
-         * @description: this is run when this thread is created and follows the ceasar cipher protocol (get rotation from client, then translate further client messages by that rotation)
-         */
-	public void run() {
+		/*
+		 * shiftString
+		 * 
+		 * @params: input, shift (rotation)
+		 * 
+		 * @returns: string that has only the letters shifted by given shift
+		 */
+		private String shiftString(String input, int shift) {
+			String shiftedString = "";
 
-	    boolean gettingRotation = true;
-	    
-	    try (DataOutputStream send = new DataOutputStream(this.socket.getOutputStream());
-	     BufferedReader recv = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));) {
+			for (int i = 0; i < input.length(); i++) {
+				
+				char character = input.charAt(i);
+				
+				if (Character.isLetter(character)) {
+					
+					int num = ((int) character);
+					int startingNumber = 0;
 
-		String results = null;
-		int rotation = -1;
-		
-		while ((results = recv.readLine()) != null) {
-		    
-		    if (gettingRotation) {
-			try {
-			    int rotationInput = Integer.parseInt(results);
-			    if (rotationInput <= 25 && rotationInput >= 1) {
-				sendMessage(send, results);
-				gettingRotation = false;
-				rotation = rotationInput;
-			    } else {
-				sendMessage(send, "Invalid rotation #, please supply a number between 1 and 25");
-			    }
-			} catch (Exception e) {
-			    sendMessage(send, "Rotation # must be an Integer!");
+					if (num >= 65 && num <= 90) { // uppercase range
+						startingNumber = 65;
+					} else { // lowercase letters
+						startingNumber = 97;
+					}
+
+					int shifted = (num + shift - startingNumber) % 26 + startingNumber;
+					character = (char) shifted;
+				}
+
+				shiftedString += character;
+
 			}
-		    } else {
-			sendMessage(send, shiftString(results, rotation));
-		    }
-
+			return shiftedString;
 		}
 
-		this.socket.close();
-		
-	    } catch (Exception e) {
-		System.out.println(e);
-	    }
-	}    
-    
-    }
+		/*
+		 * run method
+		 * 
+		 * @description: this is run when this thread is created and follows the ceasar
+		 * 	cipher protocol (get rotation from client, then translate further client
+		 * 	messages by that rotation)
+		 */
+		public void run() {
 
-    
-    // used https://www.infoworld.com/article/2853780/socket-programming-for-scalable-systems.html
-    /* main
-     * @description: starts server on specified port and creates a thread for each client connection
-     */
-    public static void main(String []args) {
-	int port = Integer.parseInt(args[0]);
-	try (ServerSocket serverSocket = new ServerSocket( port )) {
+			boolean gettingRotation = true;
 
-	    System.out.println("Listening on port " + port);
+			try (DataOutputStream send = new DataOutputStream(this.socket.getOutputStream());
+					BufferedReader recv = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));) {
 
+				String results = null;
+				int rotation = -1;
 
-	    while (true) {
-		try {
-		    Socket socket = serverSocket.accept();
-		    CeasarWorker worker = new CeasarWorker(socket);
-		    worker.start(); // have another thread handle this socket, so the server can continue to handle new users
-		} catch (Exception e) {
-		    System.out.println(e);
+				while ((results = recv.readLine()) != null) {
+
+					if (gettingRotation) {
+						try {
+							int rotationInput = Integer.parseInt(results);
+							if (rotationInput <= 25 && rotationInput >= 1) {
+								sendMessage(send, results);
+								gettingRotation = false;
+								rotation = rotationInput;
+							} else {
+								sendMessage(send, "Invalid rotation #, please supply a number between 1 and 25");
+								break;
+							}
+						} catch (Exception e) {
+							sendMessage(send, "Rotation # must be an Integer!");
+						}
+					} else {
+						sendMessage(send, shiftString(results, rotation));
+					}
+
+				}
+
+				this.socket.close();
+
+			} catch (Exception e) {
+				System.out.println(e);
+			}
 		}
-	    }
-	    
 
-	} catch (Exception e) {
-	    System.out.println(e);
 	}
-    }
-    
+
+	// Used this for reference
+	// https://www.infoworld.com/article/2853780/socket-programming-for-scalable-systems.html
+	/*
+	 * main
+	 * 
+	 * @description: starts server on specified port and creates a thread for each
+	 * client connection
+	 */
+	public static void main(String[] args) {
+		int port = Integer.parseInt(args[0]);
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+			System.out.println("Listening on port " + port);
+
+			// have another thread handle this socket, so the server
+			// can continue to handle new users
+			while (true) {
+				try {
+					Socket socket = serverSocket.accept();
+					String time = dateFormat.format(new Date());
+					System.out.println(socket.getRemoteSocketAddress().toString() + " connected at " + time);
+					CeasarWorker worker = new CeasarWorker(socket);
+					worker.start(); 
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			}
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
 }
